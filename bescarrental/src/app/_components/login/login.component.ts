076aec0d7fe67339e2185d6916e255fda097cd39
timardavid <145694962+timardavid@users.noreply.github.com>
2024-12-16 08:46:01 +0100

    Login-Panel
    A login panelnak a megjelenitése

--- bescarrental/src/app/_components/login/login.component.ts
+++ bescarrental/src/app/_components/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    
+  @Input() isVisible: boolean = false;
 
+  showPanel() {
+    this.isVisible = true;
+    console.log('Panel visible:', this.isVisible);
+  }
+  
+  closePanel() {
+    this.isVisible = false;
+    console.log('Panel visible:', this.isVisible);
+  }
+  
+
+  ngOnChanges() {
+    console.log('LoginComponent visibility changed:', this.isVisible);
+  }
 }
--- bescarrental/src/app/_components/navbar/navbar.component.ts
+++ bescarrental/src/app/_components/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, CommonModule,],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
+  @ViewChild(LoginComponent) loginComponent!: LoginComponent;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  onLoginClick() {
+    console.log('Login button clicked');
+    if (this.loginComponent) {
+      this.loginComponent.isVisible = true;
+      this.cdr.detectChanges();
+      console.log('LoginComponent isVisible:', this.loginComponent.isVisible);
+    }
+  }
 }
+
