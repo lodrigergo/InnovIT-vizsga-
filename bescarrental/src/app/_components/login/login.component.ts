1e2d6760966603ccfffac7fa7240d11b8bd75cd0
timardavid <145694962+timardavid@users.noreply.github.com>
2025-01-07 09:20:59 +0100

    login
    login elörelépés 1%

--- bescarrental/src/app/_components/login/login.component.ts
+++ bescarrental/src/app/_components/login/login.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from '../register/register.component';
-import { LoginService } from '../../_services/login.service';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, RegisterComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -68,4 +67,6 @@ export class LoginComponent {
       console.log('Form is invalid');
     }
   }
+
+  
 }
