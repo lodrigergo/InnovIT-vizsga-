33ef29006a22078ba8c5b99d0951c3b612e79b8a
HedericsNikolasz <hedericsn.23szv@acsjszki.hu>
2025-02-25 11:31:11 +0100

    athozott verzio

---
+++ InnovIT_vizsga_jo/src/main/java/com/backendvizsga/innovit_vizsga/service/BookingXCarExtrasService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backendvizsga.innovit_vizsga.service;
+
+/**
+ *
+ * @author User
+ */
+public class BookingXCarExtrasService {
+    
+}
